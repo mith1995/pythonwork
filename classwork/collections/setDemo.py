bdbd4a214c66e2a8bc112417f400228407887087
Mithlesh Pal <mithaleshpal1995@gmail.com>
2026-05-20 16:41:05 +0530

    completed the theory of the module 13

---
+++ classwork/collections/setDemo.py
@@ -0,0 +1,52 @@
+# k = {10,20,30,40,50}
+# k.add(500)
+# k.remove(100)
+# k.discard(100)
+# print(k)
+
+a = {10, 20, 30, 40, 50}
+b = {40, 50, 60, 70, 80}
+
+# a.update(b)
+# k = a.union(b)
+# k = a | b # union sign (|)
+# print(k)
+
+# a.intersection_update(b)
+# k = a.intersection(b)
+# k = a & b
+# print(k)
+
+# a.difference_update(b)
+# k = a.difference(b)
+# k = a - b
+# print(k)
+
+# a.symmetric_difference_update(b)
+# k = a.symmetric_difference(b)
+# k = a^b
+# print(k)
+
+# i = {10, 20}
+# j = {10, 20, 30, 40}
+# print(i.issubset(j))
+# print(j.issuperset(i))
+# print(i.isdisjoint(j))
+
+# l = []
+# print(type(l))
+# t = ()
+# print(type(t))
+# d = {}
+# print(type(d))
+# s = set()
+# print(s)
+
+# l = [10]
+# print(type(l))
+# t = (10,)
+# # print(t)
+# print(type(t))
+# d = {10}
+# print(type(d))
+
---
+++ classwork/collections/toupleDemo.py
@@ -0,0 +1,4 @@
+t = (10, 20, 30, 40, 10, "abc", 12.66, True)
+print(type(t))
+print(t)
+print(len(t))
